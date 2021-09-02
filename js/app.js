0638d13af7e16045aafc2a05f81937e32580bd4a
sabbir1020 <gazisabbir924@gmail.com>
2021-09-02 12:59:51 +0600

    images add

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const searchBook = ()=>{
     const bookSearch = document.getElementById('Search-book-field').value;
     // console.log(bookSearch)
     document.getElementById('Search-book-field').value ='';
-    const url  = `http://openlibrary.org/search.json?q=${bookSearch}`;
+    const url  = `https://openlibrary.org/search.json?q=${bookSearch}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayResult(data.docs))
@@ -10,16 +10,20 @@ const searchBook = ()=>{
 }
     const displayResult = docs =>{
         const searchResult = document.getElementById('search-result')
+        // clear 
+        // searchResult ='';
         docs.forEach(doc=>{
+          
             console.log(doc)
             const div = document.createElement('div')
             div.classList.add('col')
             div.innerHTML = `
             <div class="card h-100">
-               <img src="${doc.cover_i}" class="card-img-top" alt="...">
+               <img width="200px" height="400px" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Book Name:${doc.title}</h5>
                     <h5 class="card-title">Author Name:${doc.author_name}</h5>
+                    <h5 class="card-title">Publish Date:${doc.publish_date}</h5>
                 </div>
            </div>
             `;
@@ -30,12 +34,4 @@ const searchBook = ()=>{
 
 
 
-// searchBook();
-// const displayResult = results => {
-//     const searchResult = document.getElementById('search-result')
-//     results.forEach(result =>{
-//         console.log(result)
-//     })
 
-    
-// }
