56d8d43d436cfb9fd826d9a4ed3014ada08e2704
sabbir1020 <gazisabbir924@gmail.com>
2021-09-02 18:16:21 +0600

    link add

--- js/app.js
+++ js/app.js
@@ -5,19 +5,24 @@ const searchBook = ()=>{
     const url  = `https://openlibrary.org/search.json?q=${bookSearch}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => displayResult(data.docs))
+    .then(data => displayResult(data.docs ))
     // console.log(data)
 }
     const displayResult = docs =>{
         const searchResult = document.getElementById('search-result')
+      
         // clear 
         // searchResult ='';
         docs.forEach(doc=>{
           
             console.log(doc)
-            const div = document.createElement('div')
+            const div = document.createElement('div');
+            
             div.classList.add('col')
+            
             div.innerHTML = `
+               
+
             <div class="card h-100">
                <img width="200px" height="400px" src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -28,7 +33,9 @@ const searchBook = ()=>{
            </div>
             `;
             searchResult.appendChild(div)
+            
         })
+       
         // console.log(docs)
     }
 
